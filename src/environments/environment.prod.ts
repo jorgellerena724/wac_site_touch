b8c868d52af9ba05688e8a8a9f88536a8b473ea4
Ansem13 <robertov4599@gmail.com>
2026-06-12 12:50:46 -0400

    token actualizado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,6 +8,9 @@ export const environment = {
   api_security: `${apiUrl}auth/`,
   imgPath: imgPath,
   BUILD_TS: 1781276270,
-  FRONT_TOKEN:
+  /*FRONT_TOKEN:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImxpYW5hLmxvcmVuem9AdG91Y2hhbmRib29rLmNvbSIsImZ1bGxfbmFtZSI6IlRvdWNoIFJlZWYiLCJjbGllbnQiOiJ0b3VjaF9yZWVmIn0.t9Nz0jcETCBE8ZMWESdoYFDwRPMaI_zUiuui4DeE2Hg',
+*/
+  FRONT_TOKEN:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRvdWNocmVlZkBzaGlya2Fzb2Z0LmNvbSIsImZ1bGxfbmFtZSI6IlRvdWNoIFJlZWYiLCJjbGllbnQiOiJ0b3VjaF9yZWVmIn0.6kFdlePwmPaRPPot6jRs72U1yRtFxO2mBXbhwVwhe4E',
 };
